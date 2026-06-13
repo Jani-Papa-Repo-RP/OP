5424a4d67e730e2f105178ae26294d5161c1bd7b
Jani-Govind <govindtyadav207@gmail.com>
2026-06-13 21:50:05 +0530

    Update queue.py

--- PritiMusic/utils/inline/queue.py
+++ PritiMusic/utils/inline/queue.py
@@ -67,11 +67,11 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton("▉▉", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton("▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton("↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton("‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton("▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
@@ -111,7 +111,7 @@ def queuemarkup(_, vidid, chat_id):
         [
             InlineKeyboardButton(
                 text="๏ ᴍᴏʀᴇ ๏",
-                url="https://t.me/betabot_hub",
+                url="https://t.me/Jani_Ki_Jaanu",
             ),
         ],
     ]
